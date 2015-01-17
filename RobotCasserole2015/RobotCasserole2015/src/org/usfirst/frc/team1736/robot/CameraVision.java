d09879e735738913d2f3d3c028502e07450d3e1d
nzietlow <nzietlow@gmail.com>
2015-01-17 15:03:22 -0600

    Allows for two camera feeds to be merged together and lines to be drawn on the result. This allows the drivers to have input and a reference video feed.

---
+++ RobotCasserole2015/RobotCasserole2015/src/org/usfirst/frc/team1736/robot/CameraVision.java
@@ -0,0 +1,54 @@
+package org.usfirst.frc.team1736.robot;
+
+import java.nio.ByteBuffer;
+import java.nio.MappedByteBuffer;
+
+import com.ni.vision.NIVision;
+import com.ni.vision.NIVision.DrawMode;
+import com.ni.vision.NIVision.Image;
+import com.ni.vision.NIVision.Point;
+
+import edu.wpi.first.wpilibj.CameraServer;
+import edu.wpi.first.wpilibj.vision.AxisCamera;
+
+public class CameraVision 
+{
+	final String RIGHT_CAMERA_IP = "10.17.36.21";
+	final String LEFT_CAMERA_IP = "10.17.36.22";
+	
+	AxisCamera 	cameraLeft = new AxisCamera(LEFT_CAMERA_IP), 
+				cameraRight = new AxisCamera(RIGHT_CAMERA_IP);
+	
+	Image 	imageLeft = NIVision.imaqCreateImage(NIVision.ImageType.IMAGE_RGB, 0), 
+			imageRight = NIVision.imaqCreateImage(NIVision.ImageType.IMAGE_RGB, 0);
+	
+	NIVision.Rect rect;
+	NIVision.Point destLoc;
+	NIVision.Point lineStart = new NIVision.Point(10, 10);
+	NIVision.Point lineEnd = new NIVision.Point(10, 100);
+	
+	int h, w;
+	
+	
+	public void useCamera()
+	{
+		boolean freshImage = cameraLeft.isFreshImage();
+		
+		cameraLeft.getImage(imageLeft);
+		cameraRight.getImage(imageRight);
+		
+		if(freshImage)
+		{
+			h = NIVision.imaqGetImageSize(imageLeft).height;
+			w = NIVision.imaqGetImageSize(imageLeft).width;
+		}
+		
+		rect = new NIVision.Rect(0, 0, h/2, w/2);
+		destLoc = new NIVision.Point(0,0);
+		
+		NIVision.imaqCopyRect(imageRight, imageLeft, rect, destLoc);
+		NIVision.imaqDrawLineOnImage(imageRight, imageRight, DrawMode.DRAW_VALUE, lineStart, lineEnd, 5);
+		CameraServer.getInstance().setImage(imageRight);
+	}
+	
+}
