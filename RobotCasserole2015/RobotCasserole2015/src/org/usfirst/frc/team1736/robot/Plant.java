e885e7a125af4050c4b68b48990c6aa550ec3bd8
nzietlow <nzietlow@gmail.com>
2015-02-15 16:41:12 -0600

    almost done with elevator plant, still need drivetrain plant

--- RobotCasserole2015/RobotCasserole2015/src/org/usfirst/frc/team1736/robot/Plant.java
+++ RobotCasserole2015/RobotCasserole2015/src/org/usfirst/frc/team1736/robot/Plant.java
@@ -29,31 +29,32 @@ public class Plant {
 	private final double DRIVETRAIN_ROTATE_RATE_SCALER_M3 = 0.05; //converts the velocity of motor 3 into a rotation
 	
 	//elevator state
-	double cur_elevator_height = ELEVATOR_MAX_HEIGHT_IN;
-	double cur_elevator_motor_effort = 0; // range -1 to 1 for motor control
+	private double cur_elevator_height = ELEVATOR_MAX_HEIGHT_IN;
+	private double cur_elevator_motor_effort = 0; // range -1 to 1 for motor control
 	
 	//arm state
-	boolean desired_arm_state = false; //false = retracted, true = extended
-	double cur_arm_extension_distance = 0;
+	private boolean desired_arm_state = false; //false = retracted, true = extended
+	private double cur_arm_extension_distance = 0;
 	
 	//drivetrain location state
-	double cur_motor_1_effort = 0; //range -1 to 1 for motor control
-	double cur_motor_2_effort = 0;
-	double cur_motor_3_effort = 0;
+	private double cur_motor_1_effort = 0; //range -1 to 1 for motor control
+	private double cur_motor_2_effort = 0;
+	private double cur_motor_3_effort = 0;
 	
-	double cur_motor_1_encoder_distance = 0; //encoder distance readings
-	double cur_motor_2_encoder_distance = 0;
-	double cur_motor_3_encoder_distance = 0;
+	private double cur_motor_1_encoder_distance = 0; //encoder distance readings
+	private double cur_motor_2_encoder_distance = 0;
+	private double cur_motor_3_encoder_distance = 0;
 	
-	double cur_robot_x_pos = 0;
-	double cur_robot_y_pos = 0;
-	double cur_robot_pose_angle = 0; //in radians?
+	private double cur_robot_x_pos = 0;
+	private double cur_robot_y_pos = 0;
+	private double cur_robot_pose_angle = 0; //in radians?
 	
 	//current sensor readings
-	boolean elevator_bottom_sensor_state = false;
-	boolean elevator_mid_sensor_state = false;
-	boolean elevator_top_sensor_state = false;
+	private boolean elevator_bottom_sensor_state = false;
+	private boolean elevator_mid_sensor_state = false;
+	private boolean elevator_top_sensor_state = false;
 	
+	private double time_of_last_call = System.nanoTime()/1000000000;
 	
 	Plant(){ //what do we need to do on init???
 
@@ -110,28 +111,69 @@ public class Plant {
 	}
 	
 	public void setArmsSolenoidOut(){
-	
+		desired_arm_state = true;
 	}
 	
 	public void setArmsSolenoidIn(){
-		
+		desired_arm_state = false;
 	}
-	
-	public void setGrabberIn(){
-		
-	}
-	
-	public void setGrabberOut(){
-		
-	}
-	
 	
 	//Call every control loop to get updated values
 	public void evalElevatorPlant(){
+		double prev_time = time_of_last_call;
+		time_of_last_call = System.nanoTime()/1000000000;
+		double time_delta = prev_time - time_of_last_call;
+		
+		//adjust elevator height, within upper/lower limits
+		if(cur_elevator_motor_effort > 0)
+			cur_elevator_height += cur_elevator_motor_effort*ELEVATOR_MAX_DOWN_SPEED_IN_PER_SEC*time_delta;
+		else if (cur_elevator_motor_effort < 0)
+			cur_elevator_height += cur_elevator_motor_effort*ELEVATOR_MAX_UP_SPEED_IN_PER_SEC*time_delta;
+		
+		cur_elevator_height = limit_double(cur_elevator_height, ELEVATOR_MAX_HEIGHT_IN, ELEVATOR_MIN_HEIGHT_IN);
+		
+		//set elevator height sensors based on current height
+		//sensors will trigger whenever the elevator is within the magnetic sensor's detection range
+		if((cur_elevator_height < ELEVATOR_BOTTOM_SENSOR_POS_IN + MAG_SEN_DETECT_HALF_RANGE_IN) && (cur_elevator_height > ELEVATOR_BOTTOM_SENSOR_POS_IN - MAG_SEN_DETECT_HALF_RANGE_IN))
+			elevator_bottom_sensor_state = true;
+		else
+			elevator_bottom_sensor_state = false;
+		
+		if((cur_elevator_height < ELEVATOR_MID_SENSOR_POS_IN + MAG_SEN_DETECT_HALF_RANGE_IN) && (cur_elevator_height > ELEVATOR_MID_SENSOR_POS_IN - MAG_SEN_DETECT_HALF_RANGE_IN))
+			elevator_mid_sensor_state = true;
+		else
+			elevator_mid_sensor_state = false;
+		
+		if((cur_elevator_height < ELEVATOR_TOP_SENSOR_POS_IN + MAG_SEN_DETECT_HALF_RANGE_IN) && (cur_elevator_height > ELEVATOR_TOP_SENSOR_POS_IN - MAG_SEN_DETECT_HALF_RANGE_IN))
+			elevator_top_sensor_state = true;
+		else
+			elevator_top_sensor_state = false;
+		
+		//extend or retract arm.s
+		//pneumatic, so the only control is the boolean input
+		if(desired_arm_state == true)
+			cur_arm_extension_distance += ARM_MAX_EXTEND_SPEED_IN_PER_SEC*time_delta;
+		else
+			cur_arm_extension_distance += -1*ARM_MAX_RETRACT_SPEED_IN_PER_SEC*time_delta;
+		
+		cur_arm_extension_distance = limit_double(cur_arm_extension_distance, ARM_MAX_DISTANCE_IN, ARM_MIN_DISTANCE_IN);
+		
+		
+		//check for broken robot
+		if(cur_arm_extension_distance != ARM_MAX_DISTANCE_IN)
+		
+		
+		
 		
 	}
 	
 	public void evalDrivetrainPlant(){
+		double prev_time = time_of_last_call;
+		time_of_last_call = System.nanoTime()/1000000000;
+		double time_delta = prev_time - time_of_last_call;
+		
+		
+		
 		
 	}
 	
